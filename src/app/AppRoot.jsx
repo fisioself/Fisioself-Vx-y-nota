a91709e6ccbad34a02e8d3905fc37f26bb9758ad
fisioself <fisioselff@gmail.com>
2026-05-19 02:46:35 -0600

    feat: add app root composition

---
+++ src/app/AppRoot.jsx
@@ -0,0 +1,11 @@
+import { OnlineStatus } from './OnlineStatus.jsx';
+import { App } from '../App.jsx';
+
+export function AppRoot() {
+  return (
+    <>
+      <OnlineStatus />
+      <App />
+    </>
+  );
+}
