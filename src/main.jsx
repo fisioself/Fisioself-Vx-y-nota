918dd18e9f5d092ac6fe4ca8664b2f5fe2ff2179
fisioself <fisioselff@gmail.com>
2026-05-19 02:30:23 -0600

    feat: wire toast provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from './app/ErrorBoundary.jsx';
+import { ToastProvider } from './app/ToastProvider.jsx';
 import { App } from './App.jsx';
 import './styles.css';
+import './app/toasts.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
